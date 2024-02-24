ebbb5a0dc150990013050107422cfd73b53ac5bb
Albita1 <161155093+Albita1@users.noreply.github.com>
2024-02-24 18:48:18 +0100

    Add files via upload

---
+++ Alba Chacon tema 3 Tic/Actividad 1/js/js.js
@@ -0,0 +1,13 @@
+function saludar() {
+    alert("¡Holaa macacoo!");
+    emoji();
+}
+
+function depedirse(){
+    alert("bye");
+    emoji();
+}
+
+function emoji(){
+    alert("LOL");
+}
---
+++ Alba Chacon tema 3 Tic/Actividad 2/js/js.js
@@ -0,0 +1,13 @@
+function saludar(){
+    alert("Hello");
+    emoji();
+}
+
+function despedir(){
+    alert("bye");
+    emoji(":(");
+}
+
+function emoji(){
+    alert(":(");
+}
